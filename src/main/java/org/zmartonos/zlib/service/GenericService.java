2524e2e83783ca4aa8d306520182acd8e0262557
zootanka <zootanka@ztp.(none)>
2013-08-13 23:11:37 +0200

    test

--- src/main/java/org/zmartonos/zlib/service/GenericService.java
+++ src/main/java/org/zmartonos/zlib/service/GenericService.java
@@ -1,22 +1,42 @@
 package org.zmartonos.zlib.service;
 
+import org.zmartonos.zlib.utils.Utils;
+
 public class GenericService implements Runnable{
 	private int sleepInterval= 3000;
 	private boolean hasStopped= false;
+	private boolean running= false;
+	private boolean shutDown= false;
 
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		running= true;
+		while(!shutDown){
+			try {
+				work();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			Utils.sleep(sleepInterval);
+		}
 	}
 	
+	public void work()throws Exception{
+	}
+
 	public void setSleepInterval(final int sleepInterval){
 		this.sleepInterval= sleepInterval;
 	}
 	
 	public void stop(){
+		shutDown= true;
 	}
 	
 	public boolean hasStopped(){
 		return hasStopped;
+	}
+	
+	public boolean isRunning(){
+		return running;
 	}
 }
