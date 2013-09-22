5501ca524646fc4986c8a90dfb2521a972b8c950
zootanka <zootanka@ztp.(none)>
2013-09-23 01:48:11 +0200

    added io

--- src/main/java/org/zmartonos/zlib/utils/Utils.java
+++ src/main/java/org/zmartonos/zlib/utils/Utils.java
@@ -1,9 +1,14 @@
 package org.zmartonos.zlib.utils;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  * 
@@ -39,7 +44,56 @@ public class Utils {
 		return ip;
     }
 	
-	public static void sleep(final int sleepInterval){
+	public static void writeContentToFile(final String content, final String filePath) throws IOException{
+		File file= new File(filePath);
+		if(!file.getParentFile().exists())
+			file.getParentFile().mkdirs();
+
+		file.createNewFile();
+		Path path= file.toPath();
+		
+		Files.write(path, content.getBytes());
+	}
+	
+	public static boolean fileExists(final String path){
+		File file = new File(path);	
+		return file.exists();
+	}
+	
+	public static String getFileContent(final String filePath) throws IOException{
+		return new String(Files.readAllBytes(new File(filePath).toPath()));
+	}
+
+	public static String getMd5Hash(final String string){
+		return DigestUtils.md5Hex(string);
+	}
+	
+	public static String getUrlContent(String url){
+		String content= "";	
+		BufferedReader in = null;
+		try{
+			URL whatismyip = new URL(url);
+            in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+            String line;
+            while((line=in.readLine())!=null)
+            	content += line;
+        }
+		catch(Exception e){
+			e.printStackTrace();
+		} finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+		return content;
+	}
+	
+	public static void sleep(final long sleepInterval){
 		try {
 			Thread.sleep(sleepInterval);
 		} catch (InterruptedException e) {
